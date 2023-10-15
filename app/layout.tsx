f33de04564d533ba0194ef575786433c1dbc1aa4
christianlowe <christglowe@gmail.com>
2023-10-15 16:33:39 -0500

    adds nav bar to default layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import 'app/globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { AnimatePresence } from 'framer-motion';
+import AppNav from 'app/components/AppNav';
 import AppFooter from 'app/components/AppFooter';
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,6 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <AnimatePresence>
        <body className={inter.className}>
+          <AppNav />
           {children}
           <AppFooter />
         </body>
