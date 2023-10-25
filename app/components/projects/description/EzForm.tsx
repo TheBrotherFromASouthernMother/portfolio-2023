e793755f55fd026e5820f3b5ace2d7a3db41ed5c
christianlowe <christglowe@gmail.com>
2023-10-25 15:37:45 -0500

    updates formatting and language for Bidder HTX and EzForm project pages

--- app/components/projects/description/EzForm.tsx
+++ app/components/projects/description/EzForm.tsx
@@ -18,29 +18,29 @@ const EzForm = ({ project }: props): JSX.Element => (
         height={750}
         alt="Lost Title Connection advert"
     />
-    <p>
+    <p className="my-6">
         As part of the curriculum for DigitalCrafts' full stack engineering bootcamp, students were required to complete a series of
         group projects using the knowledge we accumulated to apply our technical skills.
     </p>
-    <p>
+    <p className="my-6">
         For our final group project, my group was tasked with building a product for a real-life business in order to fully apply our course-wide learnings
         in a real-world context. 
     </p>
-    <p>
-        As such, we chose to work with a local vehicle title service called "Lost Title Connection" to build an automated form filing tooling.
+    <p className="my-6">
+        As such, we chose to work with a local vehicle title service called <Link className='underline text-indigo-300 hover:text-indigo-100 ml-1 duration-500' target='_blank' rel="noopener noreferrer" href="https://www.losttitleconnection.net/">"Lost Title Connection"</Link> to build an automated form filing tooling.
     </p>
-    <p>
-        To start, we met with "Lost Title Connection" Rudy Hernandez to understand his business needs and gather intial requirements.
+    <p className="my-6">
+        To start, we met with Rudy Hernandez the owner of Lost Title Connection to understand his business needs and gather intial requirements.
         We then met as a team to discuss and negotiate scope and limitations. We then met with Rudy a second time to offer a counterpropsal.
     </p>
-    <p>
+    <p className="my-6">
         After negotiating and nailing down final requirements, we hit the whiteboards again to map out initial architecture and design.
     </p>
-    <p>
+    <p className="my-6">
         For this project our team continued our usage of vanilla JS, JQuery, and Boostrap to build out the frontend.
         However, on the backend, we chose to make use of Pytho and Django for our web framework, and PostgreSQL as our Relational Database Management System.
     </p>
-    <p>
+    <p className="my-6">
         You can view this project's source code by checking out the public repo on Github <Link className='underline text-indigo-300 hover:text-indigo-100 ml-1 duration-500' target='_blank' rel="noopener noreferrer" href="https://github.com/TheBrotherFromASouthernMother/TitleStore-Project">here.</Link>
     </p>
 </div>
